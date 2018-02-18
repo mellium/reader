6e55c30152872b487cab458c4f0e2a7285cce487
Sam Whited <sam@samwhited.com>
2018-02-17 21:27:05 -0600

    all: update copyright notice
    [ci skip]

--- reader.go
+++ reader.go
@@ -1,3 +1,7 @@
+// Copyright 2017 The Mellium Contributors.
+// Use of this source code is governed by the BSD 2-clause
+// license that can be found in the LICENSE file.
+
 // Package reader contains small, reusable APIs that build on the io.Reader
 // interface.
 package reader // import "mellium.im/reader"
